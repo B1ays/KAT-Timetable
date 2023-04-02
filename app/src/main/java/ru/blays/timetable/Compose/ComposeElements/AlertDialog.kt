bc6b76bc4bdd1bf295a10b81edd0090a6206e230
B1ays <tima.b.planshet@gmail.com>
2023-04-02 12:23:42 +0300

    Improve AlertDialog

--- app/src/main/java/ru/blays/timetable/Compose/ComposeElements/AlertDialog.kt
+++ app/src/main/java/ru/blays/timetable/Compose/ComposeElements/AlertDialog.kt
@@ -1,24 +1,68 @@
 package ru.blays.timetable.Compose.ComposeElements
 
+import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.blays.timetable.Compose.States.AlertDialogState
 
 
 @Composable
 fun CustomAlertDialog(message: String) {
+    val intent = Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS)
+    val context = LocalContext.current
     AlertDialog(
-        confirmButton = {
-            TextButton(onClick = { AlertDialogState.changeState() }) {
-            Text(text = "Ok")
+        dismissButton = {
+        TextButton(onClick = { AlertDialogState.changeState() }) {
+            Text(text = "Отмена")
         }
     },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    context.startActivity(intent)
+                    AlertDialogState.changeState()
+                }
+            )
+            {
+         Text(text = "Ок")
+    }
+    },
     onDismissRequest = { AlertDialogState.changeState() },
-    title = { Text(text = "Внимание!") },
-    text = { Text(text = message)})
+    title = { Text(text = "Не удалось подключится к сайту!", textAlign = TextAlign.Center) },
+    text = {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceAround
+        )
+        {
+            Text(
+                modifier = Modifier
+                .padding(bottom = 20.dp),
+                text = "Ошибка:\n$message"
+            )
+            Text(
+                text = "Открыть настройки сети?",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+    )
 }
+
 
 
 /*
