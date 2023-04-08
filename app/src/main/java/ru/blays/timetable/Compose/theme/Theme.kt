f721049bd65c246870b56eb23cb4b004b282a408
B1ays <tima.b.planshet@gmail.com>
2023-04-09 01:53:22 +0300

    Refactor: replace deprecated function

--- app/src/main/java/ru/blays/timetable/Compose/theme/Theme.kt
+++ app/src/main/java/ru/blays/timetable/Compose/theme/Theme.kt
@@ -1,7 +1,6 @@
 package ru.blays.timetable.Compose.theme
 
 import android.app.Activity
-import android.graphics.Color
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,9 +8,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import ru.blays.timetable.Compose.States.ThemeState.DarkColorScheme
 import ru.blays.timetable.Compose.States.ThemeState.LightColorScheme
 
@@ -54,11 +54,16 @@ fun AviakatTimetableTheme(
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
+        val currentWindow = (view.context as? Activity)?.window
+            ?: throw Exception("Not in an activity - unable to get Window reference")
+
         SideEffect {
-            (view.context as Activity).window.statusBarColor = Color.TRANSPARENT
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            currentWindow.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars =
+                darkTheme
         }
     }
+
 
     MaterialTheme(
         colorScheme = colorScheme,
