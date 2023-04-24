39511bea68a01db745944c5c835fe5c693c39b8c
B1ays <tima.b.planshet@gmail.com>
2023-04-24 19:07:05 +0300

    Bump version to 1.1.0_build7

--- data/src/main/java/ru/blays/timetable/data/models/SettingsModels/SettingsModel.kt
+++
@@ -1,9 +0,0 @@
-package ru.blays.timetable.data.models.SettingsModels
-
-data class SettingsModel(
-    val appTheme: Int? = null,
-    val monetTheme: Boolean? = null,
-    val accentColor: Int? = null,
-    val favorite: String? = null,
-    val firstStart: Boolean? = null
-)
